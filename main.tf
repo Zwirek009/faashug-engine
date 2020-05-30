4b4fee02a234e2d4b85d03ca1837c2502311ccc6
Zwirek009 <maciek.wiraszka@gmail.com>
2020-05-30 19:16:00 +0200

    add Container Registry tf

--- main.tf
+++ main.tf
@@ -7,3 +7,19 @@ terraform {
         prefix="faashug-engine"
     }
 }
+
+provider "google" {
+    project = var.project_id
+    region = var.region
+}
+
+resource "google_project_service" "container_registry" {
+    service = "containerregistry.googleapis.com"
+    disable_dependent_services = true
+}
+
+resource "google_container_registry" "container_registry" {
+    depends_on = [google_project_service.container_registry]
+    
+    location = var.multi_region_location
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+    type = string
+    default = "faashug-dev"
+}
+
+variable "region" {
+    type = string
+    default = "europe-west3"
+}
+
+variable "multi_region_location" {
+    type = string
+    default = "EU"
+}
